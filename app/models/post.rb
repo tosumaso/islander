925a0e674aa0ae560db6d1409c8463d5cd0b41c2
tosumaso <wakuwaku5712@gmail.com>
2020-12-16 14:06:14 +0900

    active-storage(仮)

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,12 @@ class Post < ApplicationRecord
   belongs_to :map
   belongs_to :evaluation
   belongs_to :user
+  has_many_attached :images
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :image_attached?
   validates :evaluation_id, presence: true, numericality: {other_than: 1}
+
+  def image_attached?
+    self.images.attached?
+  end
 end
